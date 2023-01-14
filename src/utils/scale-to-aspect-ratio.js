c8d2e00deeb76fe718a018b225db09152a879b5f
John Shaughnessy <john@johnshaughnessy.com>
2023-01-13 23:48:38 -0500

    Move function call to after definition

--- src/utils/scale-to-aspect-ratio.js
+++ src/utils/scale-to-aspect-ratio.js
@@ -1,10 +1,10 @@
-export function scaleToAspectRatio(el, ratio) {
-  scaleMeshToAspectRatio(el.object3DMap.mesh, ratio);
-}
-
 export function scaleMeshToAspectRatio(mesh, ratio) {
   const width = Math.min(1.0, 1.0 / ratio);
   const height = Math.min(1.0, ratio);
   mesh.scale.set(width, height, 1);
   mesh.matrixNeedsUpdate = true;
+}
+
+export function scaleToAspectRatio(el, ratio) {
+  scaleMeshToAspectRatio(el.object3DMap.mesh, ratio);
 }
